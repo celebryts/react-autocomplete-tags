0cd4b6b30b7496fc2ef63ea1ea6d8d40246f9e06
raphaguasta <rapha.guasta@gmail.com>
2017-06-09 01:27:30 -0300

    refactor(storybook): Update to  3.0

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { configure } from '@storybook/react'
 
 function loadStories() {
+<<<<<<< HEAD
   require('../stories/index.js')
   // You can require as many stories as you need.
+=======
+  require('../stories')
+>>>>>>> refactor(storybook): Update to  3.0
 }
 
 configure(loadStories, module)

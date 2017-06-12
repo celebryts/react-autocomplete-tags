7a5508a3eeeac2a5e92dfa4ccd31f11b4ea76470
Raphael Guastaferro <rapha.guasta@gmail.com>
2017-06-12 00:16:55 -0300

    fix(docs)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,7 @@
 import { configure } from '@storybook/react'
 
 function loadStories() {
-<<<<<<< HEAD
-  require('../stories/index.js')
-  // You can require as many stories as you need.
-=======
   require('../stories')
->>>>>>> refactor(storybook): Update to  3.0
 }
 
 configure(loadStories, module)
